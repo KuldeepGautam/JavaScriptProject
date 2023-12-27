c26e925919e345f7ff3a860f0f7ab3bd86b5ca58
Kuldeep Gautam <kuldeep1071996@gmail.com>
2023-12-27 23:56:38 +0530

    Fixed ui

---
+++ E-CommerceWebsite/app.js
@@ -0,0 +1,7 @@
+// const name = "hello world..!!";
+// console.log(name);
+
+let iconCart = document.querySelector(".icon-cart");
+let close = document.querySelector(".close");
+let body = document.querySelector("body");
+console.log(iconCart);
