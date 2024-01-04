2b34818624f8c2f74e3b9f564c8721930ba11727
kuldeep.gautam <kuldeep.gautam@sisrtd.com>
2024-01-04 17:58:02 +0530

    Added hof

--- E-CommerceWebsite/app.js
+++ E-CommerceWebsite/app.js
@@ -2,6 +2,33 @@
 // console.log(name);
 
 let iconCart = document.querySelector(".icon-cart");
-let close = document.querySelector(".close");
+let closeCard = document.querySelector(".close");
 let body = document.querySelector("body");
-console.log(iconCart);
+
+let listProductHtml = document.querySelector("list-product");
+
+let listProducts = [];
+
+iconCart.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+
+closeCard.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+// const jsonData = fs.readFileSync("products.json");
+// const fetchData = JSON.parse(jsonData);
+const fetchData = "https://jsonplaceholder.typicode.com/users";
+
+console.log(fetchData);
+function initApp() {
+  fetch(fetchData)
+    .then((response) => response.json())
+    .then(
+      (data) => console.log(data)
+      //   listProducts = data;
+    );
+}
+
+initApp();
+// console.log(iconCart);
